f9260ef7a35ae2782afaf4b84c6d7c7a9faf40af
kkimmg@gmail.com <kkimmg@gmail.com@01e661d6-a45b-11de-8a4e-6d34bb13bdfc>
2011-11-30 13:19:44 +0000

    CC = /usr/bin/gcc-4.4を使用すること

--- trunk/utils/cobpp2.bash
+++ trunk/utils/cobpp2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 ACMLIBS_DIR=${ACMLIBS_DIR:-"."}
-cobpplibs=$ACMLIBS_DIR"/acmlibs.jar"
+cobpplibs=$ACMLIBS_DIR"/sa4cob2db.jar"
 #customcodegeneratorclass="k_kim_mg.sa4cob2db.codegen.JNICodeGenerator"
 
 java -Dcustomcodegeneratorclass="k_kim_mg.sa4cob2db.codegen.JNICodeGenerator" -classpath $cobpplibs k_kim_mg.sa4cob2db.codegen.COBPP1 $1 $2 
