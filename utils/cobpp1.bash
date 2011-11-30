b0cc8b508abe2cd456e00e95961860a97328af4f
kkimmg@gmail.com <kkimmg@gmail.com@01e661d6-a45b-11de-8a4e-6d34bb13bdfc>
2011-11-30 13:19:44 +0000

    CC = /usr/bin/gcc-4.4を使用すること

--- utils/cobpp1.bash
+++ utils/cobpp1.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 ACMLIBS_DIR=${ACMLIBS_DIR:-"."}
-cobpplibs=$ACMLIBS_DIR"/acmlibs.jar"
+cobpplibs=$ACMLIBS_DIR"/sa4cob2db.jar"
 
 java -classpath $cobpplibs k_kim_mg.sa4cob2db.codegen.COBPP1 $1 $2 
